f797824c7f5c473edc5a78e9ab954e6bfadf65c7
seema1611 <seemarajpure16@gmail.com>
2020-06-21 19:15:43 +0530

    ADDED: Greeting controller to display user and counter value

---
+++ src/main/java/com/greetingapplication/GreetingapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.greetingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greetingapplication/GreetingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greetingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
